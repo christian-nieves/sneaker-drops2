8044a3327b4ac734642f6113ad2a59b453063aae
christian-nieves <thechristiannieves@gmail.com>
2026-06-09 11:26:49 -0400

    Steps 1-3 Done

--- src/main/java/com/pluralsight/sneakerdrops/Service/DropService.java
+++ src/main/java/com/pluralsight/sneakerdrops/Service/DropService.java
@@ -1,4 +1,12 @@
 package com.pluralsight.sneakerdrops.Service;
 
+import org.springframework.stereotype.Service;
+
+// Module 1: our first bean, created and managed by Spring (@Service).
+@Service
 public class DropService {
+
+    public String getStatus() {
+        return "Sneaker drops loading...";
+    }
 }
--- src/main/java/com/pluralsight/sneakerdrops/StartupRunner.java
+++ src/main/java/com/pluralsight/sneakerdrops/StartupRunner.java
@@ -1,4 +1,22 @@
 package com.pluralsight.sneakerdrops;
 
-public class StartupRunner {
+import com.pluralsight.sneakerdrops.Service.DropService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StartupRunner implements CommandLineRunner {
+
+    private final DropService dropService;
+
+    @Autowired
+    public StartupRunner(DropService dropService) {
+        this.dropService = dropService;
+    }
+
+    @Override
+    public void run(String... args) {
+        System.out.println(dropService.getStatus());
+    }
 }
